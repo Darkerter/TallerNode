d3bff74683762a96ffec001a6826064b68ac258c
Angel <lacosa784@gmail.com>
2023-09-18 18:01:44 -0600

    Actividad 8

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req,res,next)=>{
-    return res.status(200).send("Bienvenido al pokedex");
+    return res.status(200).json({code:1, message:"Bienvenido al pokedex"});
      
 });
 
 app.use("/pokemon",pokemon);
+
+app.use((req,res,next) =>{
+    return res.status(404).json({code: 404,message:"Pagina no encontrada"});
+});
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is running");
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,38 +5,38 @@ const db = require('../config/database');
 
 pokemon.get('/', async(req,res,next)=>{
     const pkmn = await  db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code:1, message:pkmn});
     
 });
 
 pokemon.post("/", (req,res,next)=>{
     
-    return res.status(200).send(req.body.name);
+    return res.status(200).json({code:1, message:req.body.name});
      
 });
 pokemon.get ('/:id([0-9]{1,3})', async(req,res,next)=>{
     const id =req.params.id -1;
-    pkmn = await  db.query("SELECT * FROM pokemon");
-    if (id >= 0 && id <= 151) 
+    const pkmn = await  db.query("SELECT * FROM pokemon WHERE pok_id="+id+";");
+    if (id >= 0 && id <= 722) 
          
-        return res.status(200).json(pkmn[req.params.id -1]);
+        return res.status(200).json({code:1, message:pkmn[req.params.id -1]});
         
-        return res.status(404).send("Pokemon no encontrado");
+        return res.status(404).send({code:404, message:"Pokemon no encontrado"});
         
      
     
 });
 pokemon.get('/:name([A-Za-z]+)', async(req,res,next)=>{
     const name =req.params.name;
-    pkmn = await  db.query("SELECT * FROM pokemon");
+    const pkmn = await  db.query("SELECT * FROM pokemon WHERE pok_id='"+name+"';");
     const pokemonFilter = pkmn.filter((p) =>{
         return (p.name.toUpperCase() == name.toUpperCase()) && p;
     }); 
     
     if(pokemonFilter.length > 0)  
-    return res.status(200).json(pokemonFilter)
+    return res.status(200).json({code:1, message:pokemonFilter})
 
-    return res.status(404).send("Pokemon no encontrado");
+    return res.status(404).send({code:404, message:"Pokemon no encontrado"});
 
 });
 module.exports = pokemon;
