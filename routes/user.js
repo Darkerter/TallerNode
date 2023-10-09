886ec2d5a813b8d0a0e823fbcc5f73b958a9b040
Angel <lacosa784@gmail.com>
2023-10-08 21:59:58 -0600

    Actividad 12

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const user = express.Router();
 const db = require('../config/database');
+const jwt = require('jsonwebtoken');
 
 user.get('/', async(req,res,next)=>{
     const user = await  db.query("SELECT * FROM user");
     return res.status(200).json({code:200, message:user});
     
 });
+user.post('/login', async(req,res,next)=>{
+    if(user_name && user_mail && user_password){
+        const{user_mail,user_password} = req.body;
+        const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password ='${user_password}'`;
+        const rows = db.query(query);
 
+        if(rows.length == 1){
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({code:201, message:token });
+        }else{
+            return res.status(400).json({code:401,message:"Usuario y/o contraseña incorrecta"});
+        }
+    }
+    return res.status(500).json({code:500,message:"Campos incompletos"});
+    
+});
 user.post('/', async(req,res,next)=>{
     const {user_name, user_mail, user_password} = req.body
 
