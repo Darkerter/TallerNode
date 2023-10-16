6566bf9f6248461ea5dcbe774b4e4704cf555abb
Angel <lacosa784@gmail.com>
2023-10-15 21:10:28 -0600

    Actividad 14

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const user = require('./routes/user');
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const index = require('./middleware/index');
+const cors = require('./middleware/cors')
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +25,6 @@ app.use("/pokemon",pokemon);
 
 app.use(notFound);
 
-app.listen(process.env.PORT || 3000,()=>{
+app.listen(process.env.PORT || 3001,()=>{
     console.log("Server is running");
 });
