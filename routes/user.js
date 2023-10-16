b9be2325cf8f34e826a41eb9e8be6826fddefc32
Angel <lacosa784@gmail.com>
2023-10-15 21:39:45 -0600

    Actividad 15

--- routes/user.js
+++ routes/user.js
@@ -28,7 +28,7 @@ user.post('/login', async(req,res,next)=>{
         return res.status(500).json({code:500,message:"Campos incompletos"});
     
 });
-user.post('/', async(req,res,next)=>{
+user.post('/signin', async(req,res,next)=>{
     const {user_name, user_mail, user_password} = req.body
 
     if(user_name && user_mail && user_password){
